b17e1137bf998cae6e1c79b8ded0a819fd1685aa
rianyunandar <rianyunandar@gmail.com>
2021-02-19 03:43:03 +0700

    link hear

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { Navbar,Nav } from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap'
 const Header = () => {
     return (
         <div>
             
    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+   <LinkContainer to ="/">
   <Navbar.Brand href=""> TV Show Apps </Navbar.Brand>
+  </LinkContainer>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     
     <Nav> 
+    <LinkContainer to ="/">
       <Nav.Link >Home</Nav.Link>
+      </LinkContainer>
+      <LinkContainer to ="/shows">
       <Nav.Link >Show</Nav.Link>
+      </LinkContainer>
       <Nav.Link >Login</Nav.Link>
       <Nav.Link >Logout</Nav.Link>
     </Nav>
